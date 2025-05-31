7419bf008e9e887b78a473679b37169c00123431
happine2s <happine2s@sookmyung.ac.kr>
2025-06-01 00:43:04 +0900

    [feat] 티어에 이모티콘 문구 추가

--- src/capstone/model/Tier.java
+++ src/capstone/model/Tier.java
@@ -1,8 +1,18 @@
 package capstone.model;
 
 public enum Tier {
-    BRONZE,
-    SILVER,
-    GOLD,
-    PLATINUM
+    BRONZE("🥉 Bronze"),
+    SILVER("🥈 Silver"),
+    GOLD("🏅 Gold"),
+    PLATINUM("🎖️Platinum");
+
+    private final String msg;
+
+    Tier(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
 }
