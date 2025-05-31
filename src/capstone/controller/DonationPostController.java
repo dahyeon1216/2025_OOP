44b7d4cc1097d64bd3fa555cbabc0148d2b00fe0
happine2s <happine2s@sookmyung.ac.kr>
2025-06-01 00:27:11 +0900

    [feat] 기부하기 기능 구현

--- src/capstone/controller/DonationPostController.java
+++ src/capstone/controller/DonationPostController.java
@@ -24,11 +24,6 @@ public class DonationPostController {
         return donationPostService.getAll();
     }
 
-    // 사용자 ID로 필터링된 기부글 조회 (추후 확장 가능)
-    public List<DonationPost> getPostsByUser(User user) {
-        return donationPostService.getByUser(user);
-    }
-
     // 기부글 수정
     public void updatePost(int id, String title, String content, String img, int goal, LocalDate endAt) {
         donationPostService.update(id, title, content, img, goal, endAt);
@@ -38,4 +33,10 @@ public class DonationPostController {
     public void deletePost(int postId) {
         donationPostService.delete(postId);
     }
+
+    // 기부글에 기부하기
+    public boolean donate(DonationPost post, User user, int donatePpoint) {
+        return donationPostService.donateToPost(post, user, donatePpoint);
+    }
+
 }
