76e6feae397908e50c1346f4a67db560d580eca2
happine2s <happine2s@sookmyung.ac.kr>
2025-06-09 12:27:35 +0900

    [design] view 관련 디자인 파일 추가

---
+++ src/capstone/view/style/RoundedBorder.java
@@ -0,0 +1,39 @@
+package capstone.view.style;
+
+//테두리 둥글하게 바꾸는 클래스 (버튼 제외)
+
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class RoundedBorder implements Border {
+    private final int radius;
+    private Color borderColor;
+
+    public RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+
+    public RoundedBorder(int radius, Color borderColor) {
+        this.radius = radius;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius/2, this.radius, this.radius/2, this.radius);
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+        return false;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // 둥글게
+        g2.setColor(Color.GRAY);
+        g2.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+        g2.dispose();
+    }
+}
